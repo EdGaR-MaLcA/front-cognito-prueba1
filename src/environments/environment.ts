bedac1b267c4b5eaa3f87b18322f99daf79d906f
Edgar <u202020371@upc.edu.pe>
2023-09-28 14:02:13 -0500

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+
+export const environment = {
+  production: false,
+  UserPoolId: 'us-east-2_tQ8Zpjo6V',// tu user pool id
+  ClientId: '5aqe8bcqtin1n8ocdnrsgdej12', // tu client id
+  springURL: 'http://localhost:8080/', // puedes cambiar la ruta o puerto donde corre tu backend.
+  ACCESS_TOKEN: 'accessToken', 
+  AUTHORIZATION: 'Authorization', 
+  BEARER: 'Bearer '
+};
